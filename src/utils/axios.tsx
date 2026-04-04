dff2df1008f28458f1ff3fcd3e8a16bf35033309
Narendrakumar Kumawat <147944938+narendrakumar9867@users.noreply.github.com>
2026-04-04 19:13:15 +0530

    Update axios.tsx

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,7 +1,9 @@
 import axios, { AxiosInstance} from "axios";
 
 const backendServerUrl = process.env.NEXT_PUBLIC_BACKEND_SERVER_URL || "";
+
 const normalizedBackendServerUrl = backendServerUrl.replace(/\/+$/, "");
+
 const hasApiSuffix = /\/api$/i.test(normalizedBackendServerUrl);
 
 export const axiosInstance: AxiosInstance = axios.create({
